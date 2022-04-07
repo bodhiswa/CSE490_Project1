89984e6b1bbd0a49d6081866b4790adfb217b7d6
Bodhiswattwa Basu <66216074+bodhiswa@users.noreply.github.com>
2022-04-07 14:27:18 -0400

    Update register_file.v

--- src/register_file.v
+++ src/register_file.v
@@ -1,8 +1,47 @@
 /* ***************************************************************\
 | Name of program :
-| Author: Jesse Both, Andrew Zhou, Ben Miller & Bodhiswattwa Basu
+| Author: 
 | Date Created:
 | Date last updated:
 | Function:
 | Modules:
 \****************************************************************/
+
+`timescale 1ns / 1ps
+
+module register_file(rd_data, rs_data, storeData, rd, rs, regSelect, immSelect, imm, write_data, CLK);
+  
+  input [7:0] write_data;
+  input rs;
+  input rd;
+  input regSelect;
+  input immSelect;
+  input CLK;
+  input [2:0] imm;
+  output reg [7:0] rs_data;
+  output reg [7:0] rd_data;
+  output reg [7:0] storeData;
+  
+  reg [7:0] RAM [0:1];
+  
+  always @(CLK) begin
+    $readmemh("registers.mem", RAM);
+    rd_data = RAM[rd];
+    rs_data = RAM[rs];
+    
+    if (immSelect == 1) begin
+        storeData = rd_data;
+        rd_data = 0; 
+        rd_data[2:0] = imm;
+    end
+  end
+  
+  always @(posedge CLK) begin
+  // if regSelect == 1, write to memory.
+    if(regSelect == 1) begin
+      RAM[rd] = write_data;
+      $writememh("registers.mem", RAM);
+      end
+    end
+endmodule
+  
