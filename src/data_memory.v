baa4855850c36c35b0629d1231b7a62d6688524f
Bodhiswattwa Basu <66216074+bodhiswa@users.noreply.github.com>
2022-04-07 14:24:28 -0400

    Update data_memory.v

--- src/data_memory.v
+++ src/data_memory.v
@@ -1,8 +1,31 @@
 /* ***************************************************************\
 | Name of program :
-| Author: Jesse Both, Andrew Zhou, Ben Miller & Bodhiswattwa Basu
+| Author: 
 | Date Created:
 | Date last updated:
 | Function:
 | Modules:
 \****************************************************************/
+
+`timescale 1ns / 1ps
+module data_memory(dataOut, select, address, storeData, CLK);
+  input [7:0] storeData; 
+  input [7:0] address;
+  input CLK;
+  input select;                // select = 0 means read. select = 1 means write.
+  output reg [7:0] dataOut;
+
+  
+  reg [7:0] RAM [255:0];
+    
+    always @(*) begin
+        $readmemh("data.mem", RAM);
+        case(select)
+            0: dataOut = RAM[address];
+            1: begin
+                RAM[address] = storeData;
+                $writememh("data.mem", RAM);
+            end
+        endcase
+    end
+endmodule
