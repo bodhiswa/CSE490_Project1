0c364064e24172cffb29b6917d20904f3c1be827
Bodhiswattwa Basu <66216074+bodhiswa@users.noreply.github.com>
2022-04-07 14:27:02 -0400

    Update prog_counter.v

--- src/prog_counter.v
+++ src/prog_counter.v
@@ -1,18 +1,34 @@
- /* ***************************************************************\
+/* ***************************************************************\
 | Name of program : 
-| Author: Jesse Both, Andrew Zhou, Ben Miller & Bodhiswattwa Basu
+| Author: 
 | Date Created:
 | Date last updated:
 | Function:
 | Modules:
 \****************************************************************/
 
+`timescale 1ns / 1ps
 
-
-// Code your design here
-
-module pc_inc(next, current);
-  input [7:0] next;
-  output [7:0] current;
-  assign next = current + 4;
+module pc_inc(next, current, jumpSelect, address, CLK);
+  output reg [7:0] next;
+  input [7:0] current;
+  input [4:0] address;
+  input jumpSelect;
+  input CLK;
+  
+  initial
+    begin
+      next = 8'b11111111;
+    end
+  
+  always @(posedge CLK) begin
+  	case(jumpSelect)
+  	1'b0: next = current;
+  	1'b1: 
+  	   begin
+  	      next = 0;
+          next[4:0] = address-1;
+       end
+    endcase
+  end
 endmodule
