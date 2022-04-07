a0f0d45e7a79b9e8bdfaa5252e4e03d86c83f5a2
Bodhiswattwa Basu <66216074+bodhiswa@users.noreply.github.com>
2022-04-07 14:27:36 -0400

    Update sign_ext.v

--- src/sign_ext.v
+++ src/sign_ext.v
@@ -4,17 +4,23 @@
 | Date Created: 03/24/2022
 | Date last updated: 03/24/2022
 | Function: Sign extend unit
-| Modules:
+| Modules: sign_ext
 \****************************************************************/
 
-module sign_ext(out, in):
+`timescale 1ns / 1ps
+
+module sign_ext(out, in);
   input in;
   output [7:0] out;
   
-  buf(out[0], in);
+  buf(out[0], in);	
   buf(out[1], in);
   buf(out[2], in);
-  
+  buf(out[3], in);
+  buf(out[4], in);
+  buf(out[5], in);
+  buf(out[6], in);
+  buf(out[7], in);
   
 endmodule
   
